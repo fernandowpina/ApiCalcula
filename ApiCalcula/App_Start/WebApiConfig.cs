e046f8c34e54538f8e8bd17991da37039a10dc4e
Fernando Pina <fernando_wesche@hotmail.com>
2021-10-23 11:09:15 -0300

    Formatando o json

--- ApiCalcula/App_Start/WebApiConfig.cs
+++ ApiCalcula/App_Start/WebApiConfig.cs
@@ -19,6 +19,9 @@ namespace ApiCalcula
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+            config.Formatters.JsonFormatter.Indent = true;
         }
     }
 }
